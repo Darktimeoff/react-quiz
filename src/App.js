0fa647d0434d17c8203ea3c5dec9419b17bd3f2e
Jevhenii <alexkorolixinofficial@gmail.com>
2020-09-06 21:55:19 +0300

    create quiz and Active quiz component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
 
 
 
 function App() {
-  return (
+	return (
         <Layout>
-			<div style={{width:400, border: '1px solid black'}}>
-				<h1>Layout works</h1>
-			</div>
+			<Quiz />
 		</Layout>
   	);
 }
