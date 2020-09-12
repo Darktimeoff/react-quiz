e39a119a2e14a23919f145a667ff4aa1bcef0cdc
Jevhenii <alexkorolixinofficial@gmail.com>
2020-09-12 11:22:23 +0300

    init redux and create base structure for redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+import ReduxThunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(ReduxThunk)));
 
 const app = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 )
 
 
